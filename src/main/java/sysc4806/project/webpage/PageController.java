45e01f917137bf04a1f05d586b757617739c23ba
lazarmilojevic <lazarsensei@gmail.com>
2022-03-08 17:00:25 -0500

    template folder structure update

--- src/main/java/sysc4806/project/webpage/PageController.java
+++ src/main/java/sysc4806/project/webpage/PageController.java
@@ -1,14 +1,14 @@
 package sysc4806.project.webpage;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Controller
-public class LandingController {
+public class PageController {
     @GetMapping("/")
-    public String landing() {
-        return "landing";
-    }
+    public String landing() { return "landing"; }
 
     @GetMapping("/login")
     public String login() { return "login"; }
@@ -21,4 +21,34 @@ public class LandingController {
 
     @GetMapping("/register")
     public String register() { return "register"; }
+
+    @GetMapping("/user")
+    public String user() { return "userView"; }
+
+    @GetMapping("/test")
+    public String test() { return "test"; }
+
+    @Bean
+    public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+        ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+        secondaryTemplateResolver.setPrefix("noUser/");
+        secondaryTemplateResolver.setSuffix(".html");
+        //secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+        secondaryTemplateResolver.setOrder(1);
+        secondaryTemplateResolver.setCheckExistence(true);
+        return secondaryTemplateResolver;
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver trialUserTemplateResolver() {
+        ClassLoaderTemplateResolver trialUserTemplateResolver = new ClassLoaderTemplateResolver();
+        trialUserTemplateResolver.setPrefix("TrialUser/");
+        trialUserTemplateResolver.setSuffix(".html");
+        //trialUserTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        trialUserTemplateResolver.setCharacterEncoding("UTF-8");
+        trialUserTemplateResolver.setOrder(1);
+        trialUserTemplateResolver.setCheckExistence(true);
+        return trialUserTemplateResolver;
+    }
 }
