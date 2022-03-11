6d61bf78d86bc107cb442573a1238cba8c414b39
RobertDeWolf7 <rdmail50@gmail.com>
2022-03-10 22:55:28 -0500

    Updated tests to provide more coverage

--- src/test/java/SpringJPA/UserTest.java
+++ src/test/java/SpringJPA/UserTest.java
@@ -10,40 +10,70 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class UserTest {
     @Autowired
     UserController controller;
 
-    User user;
+    User user, user1, user2, user3/*, user4, user5*/;
 
     @BeforeAll
     void initializeUser(){
         user = new User("Bob", "MyPass123", "123 Road");
+        user1 = new User("Joe", "MyPass1234", "1234 Road");
+        user2 = new User("Jill", "MyPass1235", "1235 Road");
+        user3 = new User("Joy", "MyPass12355", "12354 Road");
+//        user4 = new User("Reed", "MyPass123567", "123535 Road");
+//        user5 = new User("Lather", "MyPass1235224", "123578 Road");
     }
 
     @Test
-    void addAndDeleteUser(){
+    void addUser(){
         int initial = controller.getAll().size();
         controller.create(user);
-
+        assertEquals(initial+1, controller.getAll().size());
+    }
+    @Test
+    void DeleteUser(){
+        System.out.println(controller.getAll());
+        int initial = controller.getAll().size();
+        controller.create(user1);
+        System.out.println(controller.getAll());
 
         assertEquals(initial+1, controller.getAll().size());
-        controller.delete((long) initial);
+        controller.delete(5L);
+        assertNotEquals(initial+1, controller.getAll().size());
+    }
+//User user1 = new User("User1", passwordEncoder.encode("Password123"), "USER");
+    @Test
+    void getUserByID(){
+        User idUser = controller.get(1L).get(0);
+        assertEquals("User1", idUser.getUsername());
+
+        User idUser2 = controller.get(2L).get(0);
+        assertEquals("User2", idUser2.getUsername());
     }
 
     @Test
-    void userDetailsTest() {
+    void getUsers(){
+        controller.create(user3);
+        List<User> users = controller.getAll();
+        assertEquals(6, users.size());
+        assertEquals(user3.toString(), users.get(users.size()-1).toString());
+        assertNotEquals(user2.toString(), users.get(users.size()-1).toString());
 
-        controller.create(user);
-        assertEquals(user.getUsername(), "Bob");
 
-        assertEquals(controller.getByUser("Bob").toString(), user.toString());
-        assertNotEquals(controller.get(0L).toString(), user.toString());
+    }
 
-        controller.delete((long) controller.getAll().size()-1);
-
+    @Test
+    void getUserByName(){
+        controller.create(user2);
+        User fetched = controller.getByUser("Jill");
+        assertEquals(user2.toString(), fetched.toString());
+        assertNotEquals(user3.toString(), fetched.toString());
     }
 
 }
