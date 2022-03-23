690d2c3c0236c77cbf4db3876be663ad0210f6b5
pruthvichivukula <pruthvichivukula@cmail.carleton.ca>
2022-03-23 19:53:56 -0400

    devtest for calling api, not final

--- src/main/java/SpringJPA/UserController.java
+++ src/main/java/SpringJPA/UserController.java
@@ -1,10 +1,13 @@
 package SpringJPA;
 
+import SpringJPA.Model.Customer;
 import SpringJPA.Model.User;
 import SpringJPA.Model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -28,12 +31,22 @@ public class UserController {
         return userRepository.findByUsername(user);
     }
 
+
     @PostMapping("/user/json")
     public User create(@RequestBody User user) {
         return userRepository.save(user);
     }
 
-
+    @GetMapping("/user/test")
+    public String getUser(Principal principal) {
+        String ret = "";
+        User user = userRepository.findByUsername(principal.getName());
+        ret += "Before: " + user.toString() + "<br>";
+        user.incrementApICallCount();
+        userRepository.save(user);
+        ret += "After: " + user.toString();
+        return ret;
+    }
 
     @DeleteMapping("/user/json")
     public void delete(@RequestParam(value = "id") Long id) {
