429d7377240787d4291a5567ec7c39f7f1aaf729
Naman Sethi <namansethi@cmail.carleton.ca>
2022-03-24 18:58:15 -0400

    Added tests and added .idea and target/ to the .gitignore file.

---
+++ src/test/java/SpringJPA/UserPageTest.java
@@ -0,0 +1,53 @@
+package SpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserPageTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username="User1")
+    public void UserAPICallsAppears() throws Exception{
+        this.mockMvc.perform(get("/user")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("998")));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username="User1")
+    public void UserIDAppears() throws Exception{
+        this.mockMvc.perform(get("/user")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("User1")));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username="User1")
+    public void testAPICall() throws Exception{
+        this.mockMvc.perform(post("/user/makeAPICall").with(csrf()));
+        this.mockMvc.perform(get("/user")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("999")));
+    }
+}
