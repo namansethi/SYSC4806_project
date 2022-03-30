9353421cb11db77e893ab1b1053b44b9f991099a
RobertDeWolf7 <rdmail50@gmail.com>
2022-03-30 17:53:49 -0400

    merged with main

--- src/main/java/SpringJPA/WebpageApplication.java
+++ src/main/java/SpringJPA/WebpageApplication.java
@@ -30,10 +30,10 @@ public class WebpageApplication {
 		return (args) -> {
 			log.info("Logging started");
 
-			User user1 = new User("User1", passwordEncoder.encode("Password123"), "user1@gmail.com", UserType.TRIAL, Integer.toUnsignedLong(998), Integer.toUnsignedLong(1000));
-			User user2 = new User("User2", passwordEncoder.encode("123Password"), "user2@hotmail.com", UserType.TRIAL);
-			User user3 = new User("User3", passwordEncoder.encode("Pass123word"), "user3@yahoo.com", UserType.TRIAL, Integer.toUnsignedLong(0), Integer.toUnsignedLong(1000));
-			User user4 = new User("admin", passwordEncoder.encode("1Pass2word3"), "admin@admin.com", UserType.ADMIN);
+			User user1 = new User("User1", passwordEncoder.encode("Password123"), "user1@gmail.com", UserType.ROLE_TRIAL, Integer.toUnsignedLong(998), Integer.toUnsignedLong(1000));
+			User user2 = new User("User2", passwordEncoder.encode("123Password"), "user2@hotmail.com", UserType.ROLE_TRIAL);
+			User user3 = new User("User3", passwordEncoder.encode("Pass123word"), "user3@yahoo.com", UserType.ROLE_TRIAL, Integer.toUnsignedLong(0), Integer.toUnsignedLong(1000));
+			User user4 = new User("admin", passwordEncoder.encode("1Pass2word3"), "admin@admin.com", UserType.ROLE_ADMIN);
 
 			repository.save(user1);
 			repository.save(user2);
