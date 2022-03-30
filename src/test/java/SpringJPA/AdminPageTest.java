793d80bc6d31f92e52008ed35309a3b6177a304d
RobertDeWolf7 <rdmail50@gmail.com>
2022-03-30 07:46:49 -0400

    Added additional functionality to admin page, updated users to have emails.

--- src/test/java/SpringJPA/AdminPageTest.java
+++ src/test/java/SpringJPA/AdminPageTest.java
@@ -67,6 +67,20 @@ public class AdminPageTest {
         this.mockMvc.perform(get("/user/admin")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("9175")));
     }
+    @Test
+    @WithMockUser(username="admin")
+    public void testEditEmail() throws Exception{
+        this.mockMvc.perform(post("/user/admin/editEmail").param("id", "1").param("email", "testcase@gmail.com").with(csrf()));
+        this.mockMvc.perform(get("/user/admin")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("testcase@gmail.com")));
+    }
 
+    @Test
+    @WithMockUser(username="admin")
+    public void testResetCalls() throws Exception{
+        this.mockMvc.perform(post("/user/admin/resetCalls").param("id", "1").with(csrf()));
+        this.mockMvc.perform(get("/user/admin")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("0/1000")));
+    }
 
 }
